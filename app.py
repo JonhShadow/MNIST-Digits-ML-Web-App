af281090b6fb7d4935731b7f4090af3c2a7d4fa9
JonhShadow <34392955+JonhShadow@users.noreply.github.com>
2021-02-15 00:57:31 +0000

    update app

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-#web app
+# web app
 import os
 from flask import Flask, flash, redirect, url_for, render_template, request
 
-#ml
+# ml
 import keras
-from skimage.transform import resize
-import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "supertopsecretprivatekey"
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config["CACHE_TYPE"] = "null"
 
-#testing stuf
+
+# testing stuf
 @app.route('/<name>')
 def home(name):
     return f"<h1>Hello {name}</h1>"
 
+
 @app.route("/admin")
 def admin():
-    return redirect(url_for("home", name = "Joao"))
+    return redirect(url_for("home", name="Joao"))
 
 
-#start here
+# start here
 @app.route('/', methods=["POST", "GET"])
 def index():
     num = -1
@@ -43,7 +42,7 @@ def index():
                 img.save(path)
                 data = ((np.asarray(img)) / 255.0)
                 model = keras.models.load_model("digits_model.h5")
-                #pred = model.predict_classes(data.reshape(1, 28, 28))
+                # pred = model.predict_classes(data.reshape(1, 28, 28))
                 pred = model.predict_classes(data.reshape(-1, 28 * 28))
                 print(pred)
                 num = pred[0]
@@ -53,9 +52,9 @@ def index():
         else:
             print(f"Nao enviou")
             num = -2
-        
-    return render_template("index.html", pred = num, disp = img)
+
+    return render_template("index.html", pred=num, disp=img)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
